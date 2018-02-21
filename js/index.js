dc675fb2a6c8c5340bf5af67cc23c2c8c4fbf861
andrewdeutsch <andrew.deutsch@gmail.com>
2018-02-21 17:26:25 -0500

    modified as per https://gist.github.com/davoclavo/4424731 and https://github.com/graingert/datauritoblob/blob/master/dataURItoBlob.js

--- js/index.js
+++ js/index.js
@@ -170,11 +170,12 @@ function fbUpload(token){
 }
 
 function dataURItoBlob(dataURI) {
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
   var byteString = atob(dataURI.split(',')[1]);
   var ab = new ArrayBuffer(byteString.length);
   var ia = new Uint8Array(ab);
   for (var i = 0; i < byteString.length; i++) { ia[i] = byteString.charCodeAt(i); }
-  return new Blob([ab], { type: 'image/jpeg' });
+  return new Blob([ab], {type: mimeString});
 }
 
 function handleError(error) {
